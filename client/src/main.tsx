7add5405f7336ada90d2807a7d14007e2607cea8
ignit-fury <prem.patel9224@gmail.com>
2026-09-17 16:18:51 +0530

    Add Sentry error tracking to frontend

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import * as Sentry from '@sentry/react'
 import './index.css'
 import App from './App.tsx'
 
+Sentry.init({
+  dsn: "https://de6831f720428a01babf724ba8d817ed@o4512101252988928.ingest.us.sentry.io/4512101276516352",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/.*\.trycloudflare\.com/, /^https:\/\/lara-croft\.vercel\.app/],
+})
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Sentry.ErrorBoundary fallback={<div>Something went wrong.</div>}>
+      <App />
+    </Sentry.ErrorBoundary>
   </StrictMode>,
 )
